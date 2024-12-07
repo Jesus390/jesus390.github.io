964da3e2b9bf1f67b164c543b34d07f7634c4df4
Jesus390 <jesusespinola90@gmail.com>
2024-12-07 20:22:58 -0300

    Se comento la el link de navgación de 'contact me'

--- components/Navigation.js
+++ components/Navigation.js
@@ -8,7 +8,7 @@ export default function Navigation() {
                 {/* <li><a href="#portfolio">Portfolio</a></li> */}
                 {/* <li><Link href="/blog">Blog</Link></li> */}
                 <li><Link href="https://medium.com/@jesusespinola90" target="_blank">Blog</Link></li>
-                <li><Link href="#contact">Contact Me</Link></li>
+                {/* <li><Link href="#contact">Contact Me</Link></li> */}
             </ul>
         </nav>
     )
